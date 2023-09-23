00251b5af9d097c0c89a28b9958620e536a08e78
Easton <easton.fritsch21@gmail.com>
2023-09-23 14:35:52 -0600

    WIP project-router post

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -3,7 +3,7 @@ const Project = require('./projects-model')
 
 async function validateProjectId (req, res, next) {
     try {
-        const project = Project.get(req.params.id)
+        const project = await Project.get(req.params.id)
         if(!project) {
             res.status(404).json({
                 message: 'no such project',
@@ -19,6 +19,20 @@ async function validateProjectId (req, res, next) {
     }
 }
 
+function validateProject (req, res, next) {
+    const { name, description } = req.body
+    if (!name || !description) {
+        res.status(400).json({
+            message: 'missing name or description',
+        })
+    } else {
+        req.name = name
+        req.description = description
+        next()
+    }
+}
+
 module.exports = { 
     validateProjectId, 
+    validateProject,
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,20 +2,31 @@
 const express = require('express');
 const {
     validateProjectId,
-} = require('./projects-middleware')
+    validateProject,
+} = require('./projects-middleware');
+
+const Projects = require('./projects-model');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-   console.log('hello')
+router.get('/', (req, res, next) => {
+   Projects.get()
+    .then(projects => {
+        res.json(projects)
+    })
+    .catch(next)
 })
 
 router.get('/:id', validateProjectId, (req, res) => {
-    console.log('hello again')
+    res.json(req.project)
 })
 
-router.post('/', (req, res) => {
-
+router.post('/', validateProject, (req, res, next) => {
+    Projects.insert({ name: req.name, description: req.description })
+        .then(newProject => {
+            res.status(201).json(newProject)
+        })
+        .catch(next)
 })
 
 router.put('/:id', validateProjectId, (req, res) => {
@@ -28,6 +39,14 @@ router.delete('/:id', validateProjectId, (req, res) => {
 
 router.get('/:id/actions', validateProjectId, (req, res) => {
 
+})
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        customMessage: 'something didnt work in the router',
+        message: err.message,
+        stack: err.stack,
+    })
 })
 
 module.exports = router;
